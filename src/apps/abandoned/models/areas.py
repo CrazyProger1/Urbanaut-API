66c67941bcb16a7772a9bcb26e4eb4cb6309824e
crazyproger1 <crazyproger1@gmail.com>
2024-08-22 09:47:28 +0300

    add: abandoned area model fields

--- src/apps/abandoned/models/areas.py
+++ src/apps/abandoned/models/areas.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext as _
 
-from src.apps.abandoned.models.constants import AREA_NAME_MAX_LENGTH
+from src.apps.abandoned.models.constants import AREA_NAME_MAX_LENGTH, SECURITY_LEVEL_LENGTH
+from src.apps.abandoned.models.enums import SecurityLevel
+
+User = get_user_model()
 
 
 class AbandonedArea(models.Model):
@@ -9,6 +14,16 @@ class AbandonedArea(models.Model):
         verbose_name = _("Area")
         verbose_name_plural = _("Areas")
 
+    created_at = models.DateTimeField(
+        verbose_name=_("Created At"),
+        help_text=_("Object creation date and time."),
+        default=timezone.now,
+    )
+    updated_at = models.DateTimeField(
+        verbose_name=_("Updated At"),
+        help_text=_("Object updated date and time."),
+        auto_now=True,
+    )
     area = models.ForeignKey(
         "self",
         on_delete=models.CASCADE,
@@ -24,4 +39,28 @@ class AbandonedArea(models.Model):
         help_text=_("Name of the abandoned area."),
         null=False,
         blank=False,
+    )
+    description = models.TextField(
+        verbose_name=_("Description"),
+        help_text=_("Description of the abandoned area."),
+        null=True,
+        blank=True,
+    )
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="areas",
+        blank=True,
+        null=True,
+        verbose_name=_("Creator"),
+        help_text=_(""),
+    )
+    security_level = models.CharField(
+        max_length=SECURITY_LEVEL_LENGTH,
+        choices=SecurityLevel,
+        default=SecurityLevel.NONE,
+        null=False,
+        blank=False,
+        verbose_name=_("Security Level"),
+        help_text=_("Security level of the area."),
     )
