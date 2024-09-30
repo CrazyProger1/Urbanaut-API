4c08d3812a80a95a71913687e302475492793abe
crazyproger1 <crazyproger1@gmail.com>
2024-09-30 22:31:51 +0300

    add: languages api

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,10 +1,15 @@
+from django.urls import path
 from rest_framework import routers
 
-from src.apps.accounts.views import UserViewSet, TeamViewSet
+from src.apps.accounts.views import UserViewSet, TeamViewSet, get_languages
 
 router = routers.SimpleRouter()
 
 router.register("api/v1/users", UserViewSet)
 router.register("api/v1/teams", TeamViewSet)
 
-urlpatterns = [*router.urls]
+urlpatterns = [
+    path("api/v1/languages", get_languages),
+]
+
+urlpatterns += router.urls
