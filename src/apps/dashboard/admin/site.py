75a9cee511f11a81ca3beca4383d2ce5c2369e27
crazyproger1 <crazyproger1@gmail.com>
2025-01-14 13:06:08 +0200

    fix: admin_2fa_view

--- src/apps/dashboard/admin/site.py
+++ src/apps/dashboard/admin/site.py
@@ -17,20 +17,17 @@ class AdvancedAdminSite(UnfoldAdminSite):
         return response
 
     def admin_2fa_view(self, request):
+        context = {}
         if request.method == "POST":
             code = request.POST.get("code")
             key = settings.SECRET_KEY + str(request.user.id)
-            success = verify(
-                key=key,
-                code=code,
-            )
+            success = verify(key=key, code=code)
             if success:
                 request.session["admin_2fa_verified"] = True
                 return redirect(reverse("admin:index"))
             else:
-                context = {"error": "Invalid 2FA code"}
-                return render(request, self.admin_2fa_template, context)
-        return render(request, self.admin_2fa_template)
+                context["error"] = "Invalid 2FA code"
+        return render(request, self.admin_2fa_template, context)
 
     def get_urls(self):
         urls = super().get_urls()
