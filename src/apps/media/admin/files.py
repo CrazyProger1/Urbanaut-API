7ff21f539fec63c603b50c38087eb9abf8562cb4
crazyproger1 <crazyproger1@gmail.com>
2025-01-15 16:41:36 +0200

    add: files to admin page

--- src/apps/media/admin/files.py
+++ src/apps/media/admin/files.py
@@ -1,11 +1,12 @@
 from unfold.admin import ModelAdmin
 from django.contrib import admin
 
+from src.apps.dashboard.admin import site
 from src.apps.media.forms import FileForm
 from src.apps.media.models import File
 
 
-@admin.register(File)
+@admin.register(File, site=site)
 class FileAdmin(ModelAdmin):
     list_display = ("id", "creator", "type")
     list_display_links = ("creator",)
