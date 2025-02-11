57b5ac409416a2f2fc64d5729572d37235065204
crazyproger1 <crazyproger1@gmail.com>
2025-02-11 22:07:24 +0200

    add: rank key

--- src/apps/accounts/admin/users.py
+++ src/apps/accounts/admin/users.py
@@ -15,7 +15,7 @@ class UserAdmin(ModelAdmin, TabbedTranslationAdmin):
         "first_name",
         "last_name",
         "email",
-        # "rank__key",
+        "rank__key",
         "karma",
         "experience",
         "is_active",
