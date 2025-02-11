6aa14ee846c035f83dc1b3b8019ce5533c7cf03d
crazyproger1 <crazyproger1@gmail.com>
2025-02-11 21:42:08 +0200

    remove: rank enum

--- src/apps/accounts/enums.py
+++ src/apps/accounts/enums.py
@@ -5,8 +5,3 @@ from src.apps.actions.enums import ActionType
 ActionType.register("REGISTERED", "REGISTERED", _("REGISTERED"))
 ActionType.register("LOGGED_IN", "LOGGED IN", _("LOGGED IN"))
 ActionType.register("LOGGED_OUT", "LOGGED OUT", _("LOGGED OUT"))
-
-
-class UserRank(models.TextChoices):
-    NEWBIE = "NEWBIE", _("NEWBIE")
-
