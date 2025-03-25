b93f0b20be43c27a4243ae43c748716746bd903f
crazyproger1 <crazyproger1@gmail.com>
2025-03-25 22:22:58 +0200

    remove: makemigrations

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git pull
 
 poetry install --no-root
 
-poetry run python manage.py makemigrations
+#poetry run python manage.py makemigrations
 poetry run python manage.py migrate
 poetry run python manage.py collectstatic --noinput
 
