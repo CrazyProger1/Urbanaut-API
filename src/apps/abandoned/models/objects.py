db1f1bc231c790c53338285e8927626b9ee6c4f7
crazyproger1 <crazyproger1@gmail.com>
2025-03-25 21:10:58 +0200

    fix: django utils

--- src/apps/abandoned/models/objects.py
+++ src/apps/abandoned/models/objects.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from django.utis.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from mdeditor.fields import MDTextField
 
 from src.apps.abandoned.enums import (
