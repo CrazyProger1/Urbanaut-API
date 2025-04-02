bda953675df9eb6222b157727d15c47ac61578b7
crazyproger1 <crazyproger1@gmail.com>
2025-04-02 20:16:09 +0300

    imporve: atingdmin

--- src/apps/ratings/admin/ratings.py
+++ src/apps/ratings/admin/ratings.py
@@ -1,5 +1,6 @@
 from unfold.admin import ModelAdmin
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from src.apps.dashboard.admin.site import site
 from src.apps.ratings.admin.votes import RatingVoteTabularInline
@@ -10,7 +11,16 @@ from src.apps.ratings.models import Rating
 class RatingAdmin(ModelAdmin):
     list_display = (
         "id",
+        "display_value",
     )
+    list_display_links = ("display_value",)
+    readonly_fields = ("display_value",)
     inlines = (
         RatingVoteTabularInline,
     )
+
+    def display_value(self, obj: Rating):
+        stars = "⭐" * round(obj.value)
+        return stars or _("No rating")
+
+    display_value.short_description = _("Rating")
