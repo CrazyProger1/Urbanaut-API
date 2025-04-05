7964b46f68314d544e8b6c39e782da928ea7f322
crazyproger1 <crazyproger1@gmail.com>
2025-04-05 17:10:06 +0300

    fix: ewsletter

--- src/apps/notifier/models/newsletters.py
+++ src/apps/notifier/models/newsletters.py
@@ -7,14 +7,14 @@ from src.utils.db import TimestampModelMixin
 class Newsletter(TimestampModelMixin, models.Model):
     name = models.CharField(
         max_length=150,
-        blank=True,
-        null=True,
+        blank=False,
+        null=False,
         verbose_name=_("internal name"),
         help_text=_("Internal name of the newsletter."),
     )
     message = models.TextField(
-        blank=True,
-        null=True,
+        blank=False,
+        null=False,
         verbose_name=_("message"),
         help_text=_("Message of the newsletter."),
     )
