e54bc6c43f3ef32342089c83ea47ed74131e9caa
crazyproger1 <crazyproger1@gmail.com>
2025-04-05 19:35:31 +0300

    fix: notifier app

--- src/apps/notifier/admin/__init__.py
+++ src/apps/notifier/admin/__init__.py
@@ -3,3 +3,10 @@ from src.apps.notifier.admin.notifications import NotificationAdmin, Notificatio
 from src.apps.notifier.admin.newsletters import NewsletterAdmin
 from src.apps.notifier.admin.events import EventAdmin
 from src.apps.notifier.admin.categories import CategoryAdmin, CategoryRecipientInline
+from src.apps.notifier.admin.beat import (
+    SolarScheduleAdmin,
+    ClockedScheduleAdmin,
+    CrontabScheduleAdmin,
+    IntervalScheduleAdmin,
+    PeriodicTaskAdmin,
+)
