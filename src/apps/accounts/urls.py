e2699b2958540263e4bab43a4932ceec67801272
crazyproger1 <crazyproger1@gmail.com>
2025-04-05 14:17:28 +0300

    fix: settings view set registering

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -14,7 +14,10 @@ router = routers.SimpleRouter()
 router.register("api/v1/users", UserViewSet)
 router.register("api/v1/teams", TeamViewSet)
 router.register("api/v1/referral_links", ReferralLinkViewSet)
-router.register("api/v1/settings", SettingsViewSet)
+router.register("api/v1/settings", SettingsViewSet.as_view({
+    "put": "update",
+    "patch": "partial_update",
+}))
 
 urlpatterns = [
     path("api/v1/languages", get_languages),
