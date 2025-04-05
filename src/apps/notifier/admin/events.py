be8ff936a616ab84e13f66091094e4b64f597ba7
crazyproger1 <crazyproger1@gmail.com>
2025-04-05 16:35:05 +0300

    disable all inlines

--- src/apps/notifier/admin/events.py
+++ src/apps/notifier/admin/events.py
@@ -12,46 +12,46 @@ from src.apps.notifier.models import (
 )
 
 
-class NewsletterEventInline(admin.TabularInline):
-    model = NewsletterEvent
-    extra = 0
-    verbose_name = _("Newsletter")
-    verbose_name_plural = _("Newsletters")
-    tab = True
-
-
-class NotificationEventInline(admin.TabularInline):
-    model = NotificationEvent
-    extra = 0
-    verbose_name = _("Notification")
-    verbose_name_plural = _("Notifications")
-    tab = True
-
-
-class CategoryEventInline(admin.TabularInline):
-    model = CategoryEvent
-    extra = 0
-    verbose_name = _("Category")
-    verbose_name_plural = _("Categories")
-    tab = True
-
-
-class TriggerEventInline(admin.StackedInline):
-    model = TriggerEvent
-    extra = 0
-    verbose_name = _("Trigger")
-    verbose_name_plural = _("Triggers")
-    tab = True
+# class NewsletterEventInline(admin.TabularInline):
+#     model = NewsletterEvent
+#     extra = 0
+#     verbose_name = _("Newsletter")
+#     verbose_name_plural = _("Newsletters")
+#     tab = True
+#
+#
+# class NotificationEventInline(admin.TabularInline):
+#     model = NotificationEvent
+#     extra = 0
+#     verbose_name = _("Notification")
+#     verbose_name_plural = _("Notifications")
+#     tab = True
+#
+#
+# class CategoryEventInline(admin.TabularInline):
+#     model = CategoryEvent
+#     extra = 0
+#     verbose_name = _("Category")
+#     verbose_name_plural = _("Categories")
+#     tab = True
+#
+#
+# class TriggerEventInline(admin.StackedInline):
+#     model = TriggerEvent
+#     extra = 0
+#     verbose_name = _("Trigger")
+#     verbose_name_plural = _("Triggers")
+#     tab = True
 
 
 @admin.register(Event, site=site)
 class EventAdmin(ModelAdmin):
-    inlines = (
-        NewsletterEventInline,
-        NotificationEventInline,
-        CategoryEventInline,
-        TriggerEventInline,
-    )
+    # inlines = (
+    #     NewsletterEventInline,
+    #     NotificationEventInline,
+    #     CategoryEventInline,
+    #     TriggerEventInline,
+    # )
     list_display = (
         "id",
         "name",
