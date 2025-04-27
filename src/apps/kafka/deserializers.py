9d39c3ec220d7f983aa7809e3728a45e6bfc94bb
crazyproger1 <crazyproger1@gmail.com>
2025-04-27 16:29:39 +0300

    fix: JSONMessageDeserializer

--- src/apps/kafka/deserializers.py
+++ src/apps/kafka/deserializers.py
@@ -4,5 +4,6 @@ from src.apps.kafka.types import BaseMessageDeserializer
 
 
 class JSONMessageDeserializer(BaseMessageDeserializer):
-    def deserialize(self, value: bytes) -> dict:
+    @classmethod
+    def deserialize(cls, value: bytes) -> dict:
         return json.loads(value.decode("utf-8"))
