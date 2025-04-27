3db47883e108b5a4d800fe1c3ebc186f67d38bcc
crazyproger1 <crazyproger1@gmail.com>
2025-04-27 16:27:10 +0300

    fix: ReferralKafkaConsumer

--- src/apps/accounts/kafka/consumers.py
+++ src/apps/accounts/kafka/consumers.py
@@ -3,6 +3,7 @@ from src.apps.kafka.consumers import KafkaConsumer
 
 class ReferralKafkaConsumer(KafkaConsumer):
     topic = "user.referral.apply"
+    group_id = "referral_group"
 
     def handle(self, message) -> None:
         print(message, type(message), message.__dict__)
