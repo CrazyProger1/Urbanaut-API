1c508813a3d8be089d332345effb82127d3d49af
crazyproger1 <crazyproger1@gmail.com>
2025-05-04 17:34:59 +0300

    fix: default rank

--- src/apps/accounts/services/db/ranks.py
+++ src/apps/accounts/services/db/ranks.py
@@ -3,4 +3,9 @@ from src.apps.accounts.models import Rank
 
 def get_default_rank() -> Rank:
     # TODO: get default rank by min experience
-    return Rank.objects.first()
+    rank = Rank.objects.first()
+
+    if not rank:
+        rank = Rank.objects.create(key="newbie", name="newbie")
+
+    return rank
