06cd7644a0684dea59dcfc6ed7c36e8f349b6657
crazyproger1 <crazyproger1@gmail.com>
2025-05-22 23:08:36 +0300

    add: categories to abandoned object list serializer

--- src/apps/abandoned/serializers/objects.py
+++ src/apps/abandoned/serializers/objects.py
@@ -4,6 +4,7 @@ from src.apps.abandoned.models import AbandonedObject
 from src.apps.abandoned.serializers import (
     AbandonedObjectCategoryRetrieveSerializer,
     AbandonedAreaRetrieveSerializer,
+    AbandonedObjectCategoryListSerializer,
 )
 from src.apps.accounts.serializers import UserRetrieveSerializer
 from src.apps.geo.serializers import (
@@ -18,6 +19,7 @@ from src.apps.ratings.serializers import RatingRetrieveSerializer
 class AbandonedObjectListSerializer(serializers.ModelSerializer):
     photo = serializers.CharField(read_only=True)
     rating = RatingRetrieveSerializer(read_only=True)
+    categories = AbandonedObjectCategoryListSerializer(read_only=True, many=True)
 
     class Meta:
         model = AbandonedObject
@@ -38,6 +40,7 @@ class AbandonedObjectListSerializer(serializers.ModelSerializer):
             "location",
             "photo",
             "rating",
+            "categories",
         )
 
 
