7562782e12dc066872fc9e9ed986087e4e40f951
crazyproger1 <crazyproger1@gmail.com>
2025-05-24 13:43:47 +0300

    update: atingetrieveerializer

--- src/apps/ratings/services/db/votes.py
+++ src/apps/ratings/services/db/votes.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from src.apps.ratings.models import RatingVote
+from src.apps.ratings.models import RatingVote, Rating
 from src.utils.db import get_all_objects, get_object_or_none
 
 
@@ -10,3 +10,7 @@ def get_all_votes() -> models.QuerySet[RatingVote]:
 
 def get_rating_vote_or_none(**data) -> RatingVote:
     return get_object_or_none(source=RatingVote, **data)
+
+
+def get_user_vote_or_none(user, rating: Rating) -> RatingVote | None:
+    return get_rating_vote_or_none(rating=rating, created_by=user)
