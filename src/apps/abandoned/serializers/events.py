ecba3fccffb19ff94118f5815df1cd367a6272de
crazyproger1 <crazyproger1@gmail.com>
2025-05-31 15:57:10 +0300

    add: created by field to event serializer

--- src/apps/abandoned/serializers/events.py
+++ src/apps/abandoned/serializers/events.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from src.apps.abandoned.models import Event
+from src.apps.accounts.serializers import UserListSerializer, UserRetrieveSerializer
 
 
 class EventListSerializer(serializers.ModelSerializer):
+    created_by = UserListSerializer(read_only=True)
+
     class Meta:
         model = Event
         fields = (
@@ -13,10 +16,13 @@ class EventListSerializer(serializers.ModelSerializer):
             "start_at",
             "end_at",
             "status",
+            "created_by",
         )
 
 
 class EventRetrieveSerializer(serializers.ModelSerializer):
+    created_by = UserRetrieveSerializer(read_only=True)
+
     class Meta:
         model = Event
         fields = (
@@ -26,4 +32,5 @@ class EventRetrieveSerializer(serializers.ModelSerializer):
             "start_at",
             "end_at",
             "status",
+            "created_by",
         )
