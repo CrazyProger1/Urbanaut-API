96299a00a04b2da21fa85cef9243734f85e8c176
crazyproger1 <crazyproger1@gmail.com>
2025-09-30 17:29:44 +0300

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,8 @@ git checkout -- deploy.sh
 
 git pull
 
-poetry install --no-root
-
-poetry run python manage.py migrate
-poetry run python manage.py collectstatic --noinput
+uv run python manage.py migrate
+uv run python manage.py collectstatic --noinput
 
 supervisorctl restart backend
 
