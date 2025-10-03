15576f0c45fc5645fc2f2c93a72d356e7aabf28c
crazyproger1 <crazyproger1@gmail.com>
2025-10-03 16:31:19 +0300

    add: more logs

--- src/apps/accounts/views/oauth/google.py
+++ src/apps/accounts/views/oauth/google.py
@@ -27,7 +27,9 @@ class GoogleOauthRedirectURIView(APIView):
         responses=GoogleOauthRedirectURIResponseSerializer,
     )
     def get(self, request, **kwargs):
-        return Response({"redirect_uri": generate_google_oauth_redirect_uri(state=secrets.token_urlsafe(16))})
+        state = secrets.token_urlsafe(16)
+        logger.debug("Generating Google Oauth redirect URI: %s", state)
+        return Response({"redirect_uri": generate_google_oauth_redirect_uri(state=state)})
 
 
 class GoogleOauthCallbackView(APIView):
@@ -37,15 +39,18 @@ class GoogleOauthCallbackView(APIView):
         responses=GoogleOauthCallbackResponseSerializer,
     )
     def post(self, request, **kwargs):
+        logger.debug("Trying to authenticate client using Google OAuth...")
         request_serializer = GoogleOauthCallbackRequestSerializer(data=request.data)
         request_serializer.is_valid(raise_exception=True)
         code = request_serializer.validated_data["code"]
         try:
             tokens = authenticate_google_oauth_code(code=code)
         except HTTPError:
+            logger.error("Failed to authenticate the provided code or state: %s", code)
             raise AuthenticationFailed(
                 detail="Failed to authenticate the provided code or state.",
             )
         google_user = decode_id_token(tokens["id_token"])
         response_serializer = GoogleOauthCallbackResponseSerializer(instance=google_user)
+        logger.info("User authenticated via Google Oauth: %s", google_user)
         return Response(response_serializer.data, status=status.HTTP_200_OK)
