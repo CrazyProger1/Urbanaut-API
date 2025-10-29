aaa906963a335dc546dee8de837603c7a1c03d0f
crazyproger1 <crazyproger1@gmail.com>
2025-10-29 23:14:37 +0200

    feat: add logs

--- src/apps/abandoned/services/db/areas.py
+++ src/apps/abandoned/services/db/areas.py
@@ -9,6 +9,7 @@ def get_all_areas():
 def get_parent_area_or_none(area: Area, source: Source[Area] = Area) -> Area | None:
     queryset = get_queryset(source=source).exclude(id=area.id)
     areas = queryset.filter(polygon__within=area.polygon)
+    print("AREAS:", areas)
 
     for candidate in areas:
         children_ids = candidate.children.values_list("id", flat=True)
