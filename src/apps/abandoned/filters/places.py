dc7a938ada75ce1836f7f80e3d67047f38d1b3b8
crazyproger1 <crazyproger1@gmail.com>
2025-11-03 19:33:08 +0200

    fix: tags field of place filter

--- src/apps/abandoned/filters/places.py
+++ src/apps/abandoned/filters/places.py
@@ -1,11 +1,15 @@
 from django_filters import rest_framework as filters
 
 from src.apps.abandoned.models import Place
+from src.apps.tags.services.db.tags import get_all_tags
 
 
 class PlaceFilter(filters.FilterSet):
-    tags = filters.CharFilter(field_name="tags", lookup_expr="icontains")
-
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name="tags__tag",
+        to_field_name="tag",
+        queryset=get_all_tags(),
+    )
     class Meta:
         model = Place
         fields = (
