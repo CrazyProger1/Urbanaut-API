5a5c86b285d3369dd35a1cbb9455f975c9a26486
Mikolay <crazyproger1@gmail.com>
2025-11-08 11:37:37 +0000

    feat: new migrations

--- src/config/settings/databases.py
+++ src/config/settings/databases.py
@@ -1,22 +1,13 @@
 from decouple import config
 
-from src.config.settings.base import DEBUG
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": config("DB_NAME"),
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": config("DB_ENGINE", default="django.db.backends.postgresql"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT", cast=int),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": config("DB_ENGINE", default="django.db.backends.postgresql"),
-            "NAME": config("DB_NAME"),
-            "USER": config("DB_USER"),
-            "PASSWORD": config("DB_PASSWORD"),
-            "HOST": config("DB_HOST"),
-            "PORT": config("DB_PORT", cast=int),
-        }
-    }
+}
