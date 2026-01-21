776101015a054f187c897866e63416ed8a614d4f
Nikolay <crazyproger1@gmail.com>
2026-01-21 17:31:30 +0000

    feat: switch maintenance service

--- src/apps/config/admin.py
+++ src/apps/config/admin.py
@@ -1,11 +1,11 @@
 import logging
-import os
 
 from constance.forms import ConstanceForm
 from django.contrib import admin
 from constance.admin import Config, ConstanceAdmin
 
 from src.apps.accounts.sites import site
+from src.apps.config.services import switch_maintenance
 
 logger = logging.getLogger(__name__)
 
@@ -13,18 +13,9 @@ logger = logging.getLogger(__name__)
 class ConstanceConfigAdminForm(ConstanceForm):
     def save(self):
         super().save()
-        maintenance = self.cleaned_data["MAINTENANCE"]
-        try:
-            if maintenance:
-                open("/var/www/maintenance_on.flag")
-                logger.info("Maintenance mode enabled")
-            else:
-                os.remove("/var/www/maintenance_on.flag")
-                logger.info("Maintenance mode disabled")
-        except FileNotFoundError:
-            logger.warning("Maintenance flag not found")
-        except Exception as e:
-            logger.error(f"An error occurred while switching maintenance: {e}", exc_info=e)
+
+        if "MAINTENANCE" in self.cleaned_data:
+            switch_maintenance(maintenance=self.cleaned_data["MAINTENANCE"])
 
 
 @admin.register(Config, site=site)
