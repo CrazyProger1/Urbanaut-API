304946b8701eebffaeb8d9071e658f7dfccebaf8
Nikolay <crazyproger1@gmail.com>
2026-01-25 19:46:27 +0000

    feat: email provider

--- src/apps/notifications/services/providers/__init__.py
+++ src/apps/notifications/services/providers/__init__.py
@@ -1,2 +1,3 @@
 from src.apps.notifications.services.providers.types import BaseProvider
 from src.apps.notifications.services.providers.website import WebsiteProvider
+from src.apps.notifications.services.providers.email import EmailProvider
