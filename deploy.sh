599361a4aa1e56cf94fed7abb989c31b614d701c
Nikolay <crazyproger1@gmail.com>
2026-01-25 18:28:44 +0000

    feat: update deploy script to docker deploy

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,6 @@ git checkout -- deploy.sh
 
 git pull
 
-/home/admin/.local/bin/uv run python manage.py migrate
-/home/admin/.local/bin/uv run python manage.py collectstatic --noinput
-
-supervisorctl restart backend
-supervisorctl restart celery-beat
-supervisorctl restart celery-worker
-
 mkdir -p logs
+
+docker compose up --build --detach
