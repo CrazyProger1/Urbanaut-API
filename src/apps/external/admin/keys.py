59940b983ac93708dd0aa18ce516769ae2106f5b
Nikolay <crazyproger1@gmail.com>
2026-04-29 16:17:53 +0300

    feat: api key adin

--- src/apps/external/admin/keys.py
+++ src/apps/external/admin/keys.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from unfold.admin import ModelAdmin
 
 from src.apps.accounts.sites import site
@@ -11,8 +12,10 @@ class KeyAdmin(CreatedByAdminMixin, ModelAdmin):
     created_by_field = "created_by"
     list_display = (
         "name",
+        "is_revoked",
         created_by_field,
         "created_at",
+        "expired_at",
     )
     autocomplete_fields = (
         created_by_field,
@@ -20,3 +23,48 @@ class KeyAdmin(CreatedByAdminMixin, ModelAdmin):
     )
     search_fields = ("name",)
     list_filter = ("created_at",)
+
+    readonly_fields = ("key", "id", "created_at", "updated_at")
+
+    fieldsets = (
+        (
+            _("General"),
+            {
+                "fields": (
+                    "name",
+                ),
+            },
+        ),
+        (
+            _("Details"),
+            {
+                "fields": (
+                    "is_revoked",
+                ),
+            },
+        ),
+        (
+            _("Key"),
+            {
+                "fields": (
+                    "key",
+                    "id",
+                ),
+            },
+        ),
+        (
+            _("Meta"),
+            {
+                "fields": (
+                    created_by_field,
+                    "created_at",
+                    "updated_at",
+                ),
+            },
+        ),
+    )
+
+    def key(self, obj: Key):
+        return obj.key
+
+    key.short_description = "Key"
